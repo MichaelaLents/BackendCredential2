77a8b06e11dc3adc13cb7a7de9cb0fce8c8e458a
Michaela Lents <michaela.lents2@capitalone.com>
2017-12-05 15:42:03 -0500

    Adds recommendation filter

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
-let db = new Sequelize('../db/database.db', 'username', '', {
+const db = new Sequelize('../db/database.db', 'username', '', {
   dialect: 'sqlite',
   storage: './db/database.db'
   });
 
-let genres = db.define('genres', {
+const genres = db.define('genres', {
   name: Sequelize.STRING
   },
   {
     timestamps: false
   });
 
-let films = db.define('films', {
+const films = db.define('films', {
   title: Sequelize.STRING,
   release_date: Sequelize.DATE,
   genre_id: Sequelize.STRING
   },
-    {
-      timestamps: false
+  {
+    timestamps: false
   });
 
 db.sync();
@@ -54,8 +54,163 @@ function getFilmRecommendations(req, res, next) {
       err = new Error('Invalid limit');
       err.status = 422;
       return next(err);
+  }
+
+  // let recommendations = [];
+
+  films.findById(req.params.id)
+    .then(function(film){
+
+      const date = new Date(film.dataValues.release_date);
+      const year = date.getFullYear();
+      const day = date.getDate()+1;
+      const month = date.getMonth()+1;
+
+      const tempMax = month+'-'+day+'-'+(year+15);
+      const tempMin = month+'-'+day+'-'+(year-15);
+
+      const maxDate = new Date(tempMax);
+      const minDate = new Date(tempMin);
+
+      genres.findById(film.genre_id)
+        .then(function(genre){
+
+          const genreName = genre.name;
+
+          films.findAll({
+            where: {
+              genre_id: film.genre_id,
+              id:{
+                $ne: film.id
+              },
+              release_date: {
+                $between: [minDate, maxDate]
+              }
+            }
+          })
+            .then(function(films){
+
+              let results = [];
+              const filmIDs = [];
+
+              films.forEach(function(film){
+                filmIDs.push(film.id);
+              });
+
+              request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${filmIDs}`, function(error, result, body){
+                if(error){
+                  console.log('error with request: ', error)
+                  return next(error);
+                }
+
+                /*
+                  Reviews now contains all the reviews for every film that met
+                  our requirements
+                */
+                const reviews = JSON.parse(body);
+
+                reviews.forEach(function(reviewSet){
+
+                  /*
+                    Each reviewSet contains the current film id
+                    and all the reviews for that particular film
+                  */
+                  if(reviewSet.reviews.length >= 5){
+                    results = collectSignificantReviews(reviewSet, results, films, genreName);
+                  }
+
+                });  // end of   reviews.forEach
+
+                prepareAndSendResult(results, limit, offset, res);
+
+              }); // end of request
+
+          })
+          .catch(function(err){
+            console.log('Error occured trying to get all films that match our criteria. Err: ',err);
+            if(err) return next(err);
+          });
+
+      })
+      .catch(function(genErr){
+        console.log('An error occured trying to get the genre by id. Err: ', genErr);
+        if(genErr) return next(genErr);
+      });
+  })
+  .catch(function(err){
+    console.log('Error occured searching for a film by id. Err: ', err );
+    if(err) return next(err);
+  });
+
+}
+
+function prepareAndSendResult(results, limit, offset, res){
+  results.sort(function(a, b){
+    return a.id - b.id;
+  });
+
+  if(isNaN(limit)){
+    limit = 10;
+  }else{
+    limit = +limit;
+  }
+
+  if(isNaN(offset)){
+    offset = 0;
+  }else{
+    offset = +offset;
+  }
+
+  const actualResults = [];
+  let max;
+  if(results.length < (limit+offset)){
+    max = results.length;
+  }else{
+    max = limit+offset;
+  }
+  for(let i = offset; i < max; i++){
+    actualResults.push(results[i]);
+  }
+
+  res.status(200).json({
+    recommendations: actualResults,
+     meta: {
+       limit: limit,
+       offset: offset
+     }
+  });
+}
+
+function collectSignificantReviews(reviewSet, results, films, genreName){
+  let sum = 0;
+  let average = 0;
+  let count = reviewSet.reviews.length;
+
+  reviewSet.reviews.forEach(function(review){
+    sum += review.rating;
+  });
+
+  average = sum / count;
+  average = parseFloat(average).toFixed(2);
+
+  if(average >= 4.0){
+
+    let currFilm = films.find(function(film){
+      return reviewSet.film_id == film.id;
+    });
+
+    let resultFilm ={
+      id: reviewSet.film_id,
+      title: currFilm.title,
+      releaseDate: currFilm.release_date,
+      genre: genreName,
+      averageRating: average,
+      reviews: count
     }
-    
+    results.push(resultFilm);
+  }
+
+  return results;
 }
 
 /*
